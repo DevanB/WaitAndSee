68abc461434b173623bf2b6e693ad38a98b32831
Devan Beitel <devan.beitel@gmail.com>
2014-10-24 22:48:57 -0500

    Full app complete.

---
+++ js/controllers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('myApp.controllers', [])
+  .controller('LandingPageController', [function() {
+  }])
+  .controller('WaitlistController', ['$scope', 'partyService', 'textMessageService', 'authService', function($scope, partyService, textMessageService, authService) {
+    authService.getCurrentUser().then(function(user) {
+      if (user) {
+        $scope.parties = partyService.getPartiesByUserId(user.id);
+      };
+    });
+    $scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
+    $scope.saveParty = function() {
+      partyService.saveParty($scope.newParty, $scope.currentUser.id);
+      $scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
+    };
+    $scope.sendTextMessage = function(party) {
+      textMessageService.sendTextMessage(party, $scope.currentUser.id);
+    };
+  }])
+  .controller('AuthController', ['$scope', 'authService', function($scope, authService) {
+    $scope.user = {email: '', password: ''};
+    $scope.register = function() {
+      authService.register($scope.user);
+    };
+    $scope.login = function() {
+      authService.login($scope.user);
+    };
+    $scope.logout = function() {
+      authService.logout();
+    };
+  }]);
